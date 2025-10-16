0a1a933211322ec92ba7a704113c055f697e6f24
trickledown-coding <salamonstephen25@gmail.com>
2025-10-16 17:29:06 -0400

    Create taqueria.py

---
+++ Week 4 - Execptions/taqueria.py
@@ -0,0 +1,26 @@
+def main():
+    menu = {
+        "Baja Taco": 4.25,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00
+    }
+
+    total = 0
+    while True:
+        order = input("Item: ").title()
+        try:
+            for key in menu:
+                if key == order:
+                    total += menu[key]
+                    print(f"Total: ${total}")
+        except EOFError:
+            print(f"{total}")
+            break
+
+main()
