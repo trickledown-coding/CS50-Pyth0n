435eff9484e8ee4ce0544127fe5e8ef3cd4d079b
trickledown-coding <salamonstephen25@gmail.com>
2025-10-16 18:12:38 -0400

    Update grocery.py

--- Week 4 - Execptions/grocery.py
+++ Week 4 - Execptions/grocery.py
@@ -1,13 +1,25 @@
 def main():
-    grocery_list = []
+    grocery_list = {}
 
     while True:
         try:
             item = input()
-            grocery_list.append(item)
-
+            if item not in grocery_list:
+                grocery_list[item] = 1
+            elif item in grocery_list:
+                grocery_list[item] += 1
+                 
+        except KeyError:
+            pass           
         except EOFError:
-            print(grocery_list)
+            print("\n"*5)
+            print("-------------------------")
+            print("       Grocery List      ")
+            print("-------------------------")
+            for key in grocery_list:
+                print("Item: ", end='')
+                print(f"{grocery_list[key]} x ", end='')
+                print(f"{key}", end='\n')
             break
             
 
